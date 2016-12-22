8d257867b818e88edcfb638a510c22669604e1b4
Boris Besemer <borisbesemer@gmail.com>
2016-12-22 10:13:42 +0100

    removes hardcoded sessions reference

--- src/personalisation/wagtail_hooks.py
+++ src/personalisation/wagtail_hooks.py
@@ -88,7 +88,7 @@ def segment_user(page, request, serve_args, serve_kwargs):
 def serve_variation(page, request, serve_args, serve_kwargs):
     user_segments = []
 
-    for segment in request.session['segments']:
+    for segment in segments_adapter.get():
         try:
             user_segment = Segment.objects.get(pk=segment['id'],
                                                status='enabled')
