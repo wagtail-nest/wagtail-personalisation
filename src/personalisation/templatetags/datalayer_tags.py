08e40a6b61663d2c43a59b76afad2986f3d71f3f
Boris Besemer <borisbesemer@gmail.com>
2017-01-10 15:26:12 +0100

    adds datalayer template tag

---
+++ src/personalisation/templatetags/datalayer_tags.py
@@ -0,0 +1,15 @@
+from django.template import Library
+
+from personalisation.app_settings import segments_adapter
+
+register = Library()
+
+
+@register.inclusion_tag('personalisation/templates/tags/datalayer.html')
+def render_datalayer():
+    segments = segments_adapter.get_all_segments
+    segment_names = [item.name for item in segments]
+
+    return {
+        'segments': segment_names
+    }
