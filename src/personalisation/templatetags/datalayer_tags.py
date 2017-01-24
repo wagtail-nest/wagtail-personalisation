2a4aac6614a4aa074f3e5c3ed42cccb517927b7a
Jasper Berghoef <jasper.berghoef@gmail.com>
2017-01-24 14:48:20 +0100

    Fixing the datalayer tag. WIP.

--- src/personalisation/templatetags/datalayer_tags.py
+++ src/personalisation/templatetags/datalayer_tags.py
@@ -5,9 +5,9 @@ from personalisation.app_settings import segments_adapter
 register = Library()
 
 
-@register.inclusion_tag('personalisation/templates/tags/datalayer.html')
+@register.inclusion_tag('personalisation/tags/datalayer.html')
 def render_datalayer():
-    segments = segments_adapter.get_all_segments
+    segments = segments_adapter.get_all_segments()
     segment_names = [item.name for item in segments]
 
     return {
