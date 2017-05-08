47123ce7236936ef5fe3c7f0b67c52db46fdef62
blurrah <borisbesemer@gmail.com>
2017-05-08 10:10:25 +0200

    small fixes

--- src/personalisation/app_settings.py
+++ src/personalisation/app_settings.py
@@ -1,4 +1,7 @@
 from django.conf import settings
 from django.utils.module_loading import import_string
 
-segments_adapter = import_string(getattr(settings, 'PERSONALISATION_SEGMENTS_ADAPTER', 'personalisation.adapters.SessionSegmentsAdapter'))()
+segments_adapter = import_string(getattr(
+    settings,
+    'PERSONALISATION_SEGMENTS_ADAPTER',
+    'personalisation.adapters.SessionSegmentsAdapter'))()
