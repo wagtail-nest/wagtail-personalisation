b62fabd47bf4b960167e784c83a01906d3a99b3a
Michael van Tellingen <michael@mvantellingen.nl>
2017-05-08 09:57:27 +0200

    Fix PROJECT_DIR path in te sandbox

--- sandbox/sandbox/settings.py
+++ sandbox/sandbox/settings.py
@@ -15,7 +15,7 @@ from __future__ import absolute_import, unicode_literals
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
-PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
 
 DEBUG = True
