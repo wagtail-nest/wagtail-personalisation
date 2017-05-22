885b378b63a212aee15cb22ae35b03f0337c02f0
Jasper Berghoef <jasper.berghoef@gmail.com>
2017-05-22 15:38:48 +0200

    Improve comments and readability for several files
    App settings: added a comment.
    Admin: consistent docstrings.
    Admin urls: better readability.

--- src/personalisation/admin.py
+++ src/personalisation/admin.py
@@ -6,29 +6,39 @@ from personalisation import models, rules
 
 
 class UserIsLoggedInRuleAdminInline(admin.TabularInline):
-    """Inline the UserIsLoggedIn Rule into the administration interface for segments"""
+    """Inline the UserIsLoggedIn Rule into the
+    administration interface for segments.
+    
+    """
     model = rules.UserIsLoggedInRule
 
 
 class TimeRuleAdminInline(admin.TabularInline):
-    """Inline the Time Rule into the administration interface for segments"""
+    """Inline the Time Rule into the
+    administration interface for segments.
+    
+    """
     model = rules.TimeRule
 
 
 class ReferralRuleAdminInline(admin.TabularInline):
     """Inline the Referral Rule into the
-    administration interface for segments"""
+    administration interface for segments.
+    
+    """
     model = rules.ReferralRule
 
 
 class VisitCountRuleAdminInline(admin.TabularInline):
     """Inline the Visit Count Rule into the
-    administration interface for segments"""
+    administration interface for segments.
+    
+    """
     model = rules.VisitCountRule
 
 
 class SegmentAdmin(admin.ModelAdmin):
-    """Add the inlines to the Segment admin interface"""
+    """Add the inline models to the Segment admin interface."""
     inlines = (UserIsLoggedInRuleAdminInline, TimeRuleAdminInline,
                ReferralRuleAdminInline, VisitCountRuleAdminInline)
 
--- src/personalisation/admin_urls.py
+++ src/personalisation/admin_urls.py
@@ -5,11 +5,12 @@ from django.conf.urls import url
 from personalisation import views
 
 app_name = 'segment'
+
 urlpatterns = [
-    url(r'^segment/(?P<segment_id>[0-9]+)/enable/$', views.enable,
-        name='enable'),
-    url(r'^segment/(?P<segment_id>[0-9]+)/disable/$', views.disable,
-        name='disable'),
+    url(r'^segment/(?P<segment_id>[0-9]+)/enable/$',
+        views.enable, name='enable'),
+    url(r'^segment/(?P<segment_id>[0-9]+)/disable/$',
+        views.disable, name='disable'),
     url(r'^(?P<page_id>[0-9]+)/copy/(?P<segment_id>[0-9]+)$',
         views.copy_page_view, name='copy_page')
 ]
