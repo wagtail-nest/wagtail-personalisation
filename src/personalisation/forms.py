974a4d6f465d0b96898d3171762c6e8e9fa01ac0
Jasper Berghoef <jasper.berghoef@gmail.com>
2017-05-24 17:34:34 +0200

    Moves the form in to models.py to forms.py

---
+++ src/personalisation/forms.py
@@ -0,0 +1,45 @@
+from wagtail.wagtailadmin.forms import WagtailAdminPageForm
+
+
+class AdminPersonalisablePageForm(WagtailAdminPageForm):
+    """The personalisable page form that allows creation of variants."""
+    def __init__(self, *args, **kwargs):
+        super(AdminPersonalisablePageForm, self).__init__(*args, **kwargs)
+
+    def save(self, commit=True):
+        """Save a copy of the original page, linked to a segment.
+
+        :returns: The original page, or a new page.
+        :rtype: personalisation.models.PersonalisablePage
+
+        """
+        page = super(AdminPersonalisablePageForm, self).save(commit=False)
+
+        if page.segment:
+            segment = page.segment
+            slug = "{}-{}".format(page.slug, segment.encoded_name())
+            title = "{} ({})".format(page.title, segment.name)
+            update_attrs = {
+                'title': title,
+                'slug': slug,
+                'segment': segment,
+                'live': False,
+                'canonical_page': page,
+                'is_segmented': True,
+            }
+
+            if page.is_segmented:
+                slug = "{}-{}".format(
+                    page.canonical_page.slug, segment.encoded_name())
+                title = "{} ({})".format(
+                    page.canonical_page.title, segment.name)
+                page.slug = slug
+                page.title = title
+                page.save()
+                return page
+            else:
+                new_page = page.copy(
+                    update_attrs=update_attrs, copy_revisions=False)
+                return new_page
+
+        return page
--- src/personalisation/models.py
+++ src/personalisation/models.py
@@ -12,9 +12,9 @@ from wagtail.utils.decorators import cached_classmethod
 from wagtail.wagtailadmin.edit_handlers import (
     FieldPanel, FieldRowPanel, InlinePanel, MultiFieldPanel, ObjectList,
     PageChooserPanel, TabbedInterface)
-from wagtail.wagtailadmin.forms import WagtailAdminPageForm
 from wagtail.wagtailcore.models import Page
 
+from personalisation.forms import AdminPersonalisablePageForm
 from personalisation.rules import AbstractBaseRule
 
 
@@ -93,50 +93,6 @@ def check_status_change(sender, instance, *args, **kwargs):
 
 
 pre_save.connect(check_status_change, sender=Segment)
-
-
-class AdminPersonalisablePageForm(WagtailAdminPageForm):
-    """The personalisable page form that allows creation of variants."""
-    def __init__(self, *args, **kwargs):
-        super(AdminPersonalisablePageForm, self).__init__(*args, **kwargs)
-
-    def save(self, commit=True):
-        """Save a copy of the original page, linked to a segment.
-
-        :returns: The original page, or a new page.
-        :rtype: personalisation.models.PersonalisablePage
-
-        """
-        page = super(AdminPersonalisablePageForm, self).save(commit=False)
-
-        if page.segment:
-            segment = page.segment
-            slug = "{}-{}".format(page.slug, segment.encoded_name())
-            title = "{} ({})".format(page.title, segment.name)
-            update_attrs = {
-                'title': title,
-                'slug': slug,
-                'segment': segment,
-                'live': False,
-                'canonical_page': page,
-                'is_segmented': True,
-            }
-
-            if page.is_segmented:
-                slug = "{}-{}".format(
-                    page.canonical_page.slug, segment.encoded_name())
-                title = "{} ({})".format(
-                    page.canonical_page.title, segment.name)
-                page.slug = slug
-                page.title = title
-                page.save()
-                return page
-            else:
-                new_page = page.copy(
-                    update_attrs=update_attrs, copy_revisions=False)
-                return new_page
-
-        return page
 
 
 class PersonalisablePage(Page):
