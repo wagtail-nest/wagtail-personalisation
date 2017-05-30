33cf0217da5a753836863c53993da9f14e3c8fba
blurrah <borisbesemer@gmail.com>
2017-05-30 17:36:32 +0200

    fixes page crashing when visiting a page that doesnt inherit PersonalisablePage

--- src/wagtail_personalisation/wagtail_hooks.py
+++ src/wagtail_personalisation/wagtail_hooks.py
@@ -141,14 +141,13 @@ def _check_for_variations(segments, page):
     """
     for segment in segments:
         page_class = page.__class__
-        if not any(item == PersonalisablePage for item in page_class.__bases__):
-            page_class = PersonalisablePage
+        if any(item == PersonalisablePage for item in page_class.__bases__):
 
-        variation = page_class.objects.filter(
-            canonical_page=page, segment=segment)
+            variation = page_class.objects.filter(
+                canonical_page=page, segment=segment)
 
-        if variation:
-            return variation
+            if variation:
+                return variation
 
     return None
 
