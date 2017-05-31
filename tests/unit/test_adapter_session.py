5eefa21699f2dc7b75a919b5899a25ec7ef5c5b7
Michael van Tellingen <michael@mvantellingen.nl>
2017-05-31 16:18:32 +0200

    Add test for sessionadapter.refresh when segment is disable

--- tests/unit/test_adapter_session.py
+++ tests/unit/test_adapter_session.py
@@ -33,3 +33,22 @@ def test_get_segment_by_id(rf, monkeypatch):
 
     segment_x = adapter.get_segment_by_id(segment_2.pk)
     assert segment_x == segment_2
+
+
+@pytest.mark.django_db
+def test_refresh_removes_disabled(rf, monkeypatch):
+    request = rf.get('/')
+
+    adapter = adapters.SessionSegmentsAdapter(request)
+
+    segment_1 = SegmentFactory(name='segment-1', persistent=True)
+    segment_2 = SegmentFactory(name='segment-2', persistent=True)
+
+    adapter.set_segments([segment_1, segment_2])
+
+    adapter = adapters.SessionSegmentsAdapter(request)
+    segment_1.status = segment_1.STATUS_DISABLED
+    segment_1.save()
+    adapter.refresh()
+
+    assert adapter.get_segments() == [segment_2]
