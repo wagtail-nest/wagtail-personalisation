cf8101156c35b3f3cfb54bd141fdb5fcd67dfc36
Michael van Tellingen <michael@mvantellingen.nl>
2017-05-31 21:44:50 +0200

    Extend the sandbox home to show text content

--- sandbox/sandbox/apps/home/models.py
+++ sandbox/sandbox/apps/home/models.py
@@ -1,9 +1,15 @@
 from __future__ import absolute_import, unicode_literals
 
 from wagtail.wagtailcore.models import Page
+from wagtail.wagtailadmin.edit_handlers import RichTextFieldPanel
+from wagtail.wagtailcore.fields import RichTextField
 
 from wagtail_personalisation.models import PersonalisablePageMixin
 
 
 class HomePage(PersonalisablePageMixin, Page):
-    pass
+    text_content = RichTextField()
+
+    content_panels = Page.content_panels + [
+        RichTextFieldPanel('text_content')
+    ]
