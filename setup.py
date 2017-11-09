9d1f3074c08e202f8ab258f654bad014d96055f9
Milton Madanda <madandat@gmail.com>
2017-11-09 13:18:22 +0200

    add pypi password

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ with open('README.rst') as fh:
 
 setup(
     name='wagtail-personalisation-molo',
-    version='0.10.0beta1',
+    version='0.10.0',
     description='A forked version of Wagtail add-on for showing personalized content',
     author='Praekelt.org',
     author_email='dev@praekeltfoundation.org',
