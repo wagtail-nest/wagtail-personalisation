362f15e5ffdce5112837eaf9a058eb17094d2f61
Saeed Marzban <Saeed.mb8@gmail.com>
2018-02-13 14:17:29 +0200

    version bump 0.10.9

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ with open('README.rst') as fh:
 
 setup(
     name='wagtail-personalisation-molo',
-    version='0.10.8',
+    version='0.10.9',
     description='A forked version of Wagtail add-on for showing personalized content',
     author='Praekelt.org',
     author_email='dev@praekeltfoundation.org',
