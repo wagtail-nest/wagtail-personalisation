f6598ca1f70f06f321457d0f5d522cf96cfbe2ec
Jasper Berghoef <jasper.berghoef@gmail.com>
2018-05-26 12:55:29 +0200

    Adjust requirements

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ import re
 from setuptools import find_packages, setup
 
 install_requires = [
-    'wagtail>=2.0,<2.1',
-    'user-agents>=1.0.1',
+    'wagtail>=2.0,<2.2',
+    'user-agents>=1.1.0',
     'wagtailfontawesome>=1.1.3',
 ]
 
