c31415b484cf82dc3b9182ce6201a548b1f1201f
Jasper Berghoef <jasper.berghoef@gmail.com>
2018-05-31 12:08:40 +0200

    Fix migration ordering

--- src/wagtail_personalisation/migrations/0021_segment_status_as_boolean.py
+++ src/wagtail_personalisation/migrations/0021_segment_status_as_boolean.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('wagtail_personalisation', '0012_remove_personalisablepagemetadata_is_segmented'),
+        ('wagtail_personalisation', '0020_rules_delete_relatedqueryname'),
     ]
 
     operations = [
