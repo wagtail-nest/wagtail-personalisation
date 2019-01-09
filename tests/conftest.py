c07b28027670174e19da055180451257517bbf25
sewagodimo <edneciamat@gmail.com>
2019-01-09 16:55:06 +0200

    allow database access for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,10 @@ pytest_plugins = [
     'tests.fixtures'
 ]
 
+@pytest.fixture(autouse=True)
+def enable_db_access(db):
+    pass
+
 
 @pytest.fixture(scope='session')
 def django_db_setup(django_db_setup, django_db_blocker):
