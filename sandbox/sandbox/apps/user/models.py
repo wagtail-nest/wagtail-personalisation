13e13ccae543b0e5400f5962cd71ccb22c311017
Paul J Stevens <p.stevens@labdigital.nl>
2019-03-15 13:53:05 +0100

    update sandbox

--- sandbox/sandbox/apps/user/models.py
+++ sandbox/sandbox/apps/user/models.py
@@ -1,14 +1,44 @@
 from django.contrib.auth.models import (
-    AbstractBaseUser, PermissionsMixin, UserManager)
+    AbstractBaseUser, PermissionsMixin, BaseUserManager)
 from django.core.mail import send_mail
-from django.db import connections, models
-from django.dispatch import receiver
+from django.db import models
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
 
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Create and save a user with the given username, email, and password.
+        """
+        if not email:
+            raise ValueError('The given email address must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self._create_user(email, password, **extra_fields)
+
+
 class User(AbstractBaseUser, PermissionsMixin):
-    """Cusomtized version of the default `AbstractUser` from Django.
+    """Customized version of the default `AbstractUser` from Django.
 
     """
     first_name = models.CharField(_('first name'), max_length=100, blank=True)
--- sandbox/sandbox/urls.py
+++ sandbox/sandbox/urls.py
@@ -11,9 +11,9 @@ from wagtail.documents import urls as wagtaildocs_urls
 from sandbox.apps.search import views as search_views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^django-admin/', admin.site.urls),
 
-    url(r'^cms/', include(wagtailadmin_urls)),
+    url(r'^admin/', include(wagtailadmin_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
 
     url(r'^search/$', search_views.search, name='search'),
