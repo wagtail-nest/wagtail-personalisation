16e24b67918e242148a479d69a14640a6479c509
Boris Besemer <borisbesemer@gmail.com>
2021-07-13 17:01:35 +0200

    Bump version: 0.15.0 → 0.15.1

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ with open('README.rst') as fh:
 
 setup(
     name='wagtail-personalisation',
-    version='0.15.0',
+    version='0.15.1',
     description='A Wagtail add-on for showing personalized content',
     author='Lab Digital BV and others',
     author_email='opensource@labdigital.nl',
