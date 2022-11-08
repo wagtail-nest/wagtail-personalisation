4e2f4605c58eefada6e59f43b9f1726720c1ec70
nick.moreton <nick.moreton@torchbox.com>
2022-11-08 13:56:45 +0000

    Update missing Wagtail 3 version conditionals

--- sandbox/sandbox/urls.py
+++ sandbox/sandbox/urls.py
@@ -4,8 +4,14 @@ import debug_toolbar
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, re_path
+from wagtail import VERSION as WAGTAIL_VERSION
 from wagtail.admin import urls as wagtailadmin_urls
-from wagtail.core import urls as wagtail_urls
+
+if WAGTAIL_VERSION >= (3, 0):
+    from wagtail.urls import urls as wagtail_urls
+else:
+    from wagtail.core import urls as wagtail_urls
+    
 from wagtail.documents import urls as wagtaildocs_urls
 
 from sandbox.apps.search import views as search_views
