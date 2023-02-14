92cc08884cfce3f8968a2e718a4aa749a5dd4f37
Nick Moreton <nick.moreton@torchbox.com>
2023-02-14 09:45:49 +0000

    Remove support for wagtail < 4.1 in sandbox

--- sandbox/sandbox/apps/home/models.py
+++ sandbox/sandbox/apps/home/models.py
@@ -1,17 +1,9 @@
 from __future__ import absolute_import, unicode_literals
 
-from wagtail import VERSION as WAGTAIL_VERSION
-
-if WAGTAIL_VERSION >= (3, 0):
-    from wagtail import blocks
-    from wagtail.admin.panels import FieldPanel
-    from wagtail.fields import RichTextField, StreamField
-    from wagtail.models import Page
-else:
-    from wagtail.admin.edit_handlers import RichTextFieldPanel, StreamFieldPanel
-    from wagtail.core import blocks
-    from wagtail.core.fields import RichTextField, StreamField
-    from wagtail.core.models import Page
+from wagtail import blocks
+from wagtail.admin.panels import FieldPanel
+from wagtail.fields import RichTextField, StreamField
+from wagtail.models import Page
 
 from wagtail_personalisation.blocks import PersonalisedStructBlock
 from wagtail_personalisation.models import PersonalisablePageMixin
@@ -19,39 +11,22 @@ from wagtail_personalisation.models import PersonalisablePageMixin
 
 class HomePage(PersonalisablePageMixin, Page):
     intro = RichTextField()
-    if WAGTAIL_VERSION >= (3, 0):
-        body = StreamField(
-            [
-                (
-                    "personalisable_paragraph",
-                    PersonalisedStructBlock(
-                        [
-                            ("paragraph", blocks.RichTextBlock()),
-                        ],
-                        icon="pilcrow",
-                    ),
-                )
-            ],
-            use_json_field=True,
-        )
-    else:
-        body = StreamField(
-            [
-                (
-                    "personalisable_paragraph",
-                    PersonalisedStructBlock(
-                        [
-                            ("paragraph", blocks.RichTextBlock()),
-                        ],
-                        icon="pilcrow",
-                    ),
-                )
-            ]
-        )
+    body = StreamField(
+        [
+            (
+                "personalisable_paragraph",
+                PersonalisedStructBlock(
+                    [
+                        ("paragraph", blocks.RichTextBlock()),
+                    ],
+                    icon="pilcrow",
+                ),
+            )
+        ],
+        use_json_field=True,
+    )
 
     content_panels = Page.content_panels + [
-        FieldPanel("intro")
-        if WAGTAIL_VERSION >= (3, 0)
-        else RichTextFieldPanel("intro"),
-        FieldPanel("body") if WAGTAIL_VERSION >= (3, 0) else StreamFieldPanel("body"),
+        FieldPanel("intro"),
+        FieldPanel("body"),
     ]
