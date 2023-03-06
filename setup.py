684704128d1324ed90c5648ee825f0f431853e9c
Nick Moreton <nick.moreton@torchbox.com>
2023-03-06 13:01:44 +0000

    revert the wagtail-font-awesome-svg dependency to wagtailfontawesome

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 install_requires = [
     "wagtail>=4.1",
     "user-agents>=1.1.0",
-    "wagtail-font-awesome-svg>=0.0.3",
+    "wagtailfontawesome>=1.2.1",
     "pycountry",
 ]
 
